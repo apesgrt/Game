9390148f4055ce89bd4f74a7825ec1713731b452
Raffz <apesmiis@gmail.com>
2026-06-11 14:29:12 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAAHiHYuCFfRk-UkUMDCrGgNIIpZQGN6Go",
+  authDomain: "peswebb-98af2.firebaseapp.com",
+  databaseURL: "https://peswebb-98af2-default-rtdb.firebaseio.com",
+  projectId: "peswebb-98af2",
+  storageBucket: "peswebb-98af2.firebasestorage.app",
+  messagingSenderId: "876898601670",
+  appId: "1:876898601670:web:d7267d232f81ad9c45e5c7",
+  measurementId: "G-QY7DRCCD7Q"
+};
